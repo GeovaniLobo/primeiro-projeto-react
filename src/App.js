abf269d2766c6603cac78dc8d2a7925442d44984
GeovaniLobo <geovanilobo98@gmail.com>
2024-02-14 13:52:03 -0300

    Adicionando arquivos iniciais e removendo arquivos desnecessários

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Container, H1, Image, ContainerItens, Input, InputLabel, Button } from "./styles";
+
+function App() {
+
+  return (
+    <Container>
+      <Image />
+      <ContainerItens>
+      <H1>Olá!</H1>
+      <InputLabel>Nome</InputLabel>
+      <Input placeholder='Nome' />
+      <InputLabel>Idade</InputLabel>
+      <Input placeholder='Idade' />
+      <Button>Cadastrar</Button>
+      </ContainerItens>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+`;
+
+export const Image = styled.img`
+`;
+
+export const ContainerItens = styled.div`
+`;
+
+export const H1 = styled.h1`
+color: blue;
+font-size: 20px;
+`;
+
+export const InputLabel = styled.p`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
